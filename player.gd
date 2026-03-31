5df9a8ac925d23af8841398f206668ce3dc07ba6
Kumail Abbas <156226608+komi-ai@users.noreply.github.com>
2026-03-31 12:16:56 -0400

    prototype start
    made basic player movement

---
+++ player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var speed = 200
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	
+	velocity = direction.normalized() * speed
+	move_and_slide()
