28cc49e38981a50b1be9db93d4689732e721ecae
Kumail Abbas <156226608+komi-ai@users.noreply.github.com>
2026-04-07 11:49:17 -0400

    01
    updated

---
+++ scripts/main_ui.gd
@@ -0,0 +1,41 @@
+extends Control
+
+var money = 0
+var inventory = []
+var max_items = 4
+
+func update_ui():
+	$MoneyLabel.text = "Money: " + str(money)
+
+	if inventory.size() == 0:
+		$InventoryLabel.text = "Inventory: Empty"
+	else:
+		$InventoryLabel.text = "Inventory: " + ", ".join(inventory)
+
+func _ready():
+	update_ui()
+	$MessageLabel.text = "Click a cave to gather an item."
+
+func add_item(item_name):
+	if inventory.size() < max_items:
+		inventory.append(item_name)
+		$MessageLabel.text = "You found a " + item_name + "."
+	else:
+		$MessageLabel.text = "Inventory Full."
+
+	update_ui()
+
+func _on_cave_button1_pressed():
+	add_item("Stone")
+
+func _on_cave_button2_pressed():
+	add_item("Bone")
+
+func _on_drop_button_pressed():
+	if inventory.size() > 0:
+		var removed_item = inventory.pop_back()
+		$MessageLabel.text = "Dropped " + removed_item + "."
+	else:
+		$MessageLabel.text = "Inventory is empty."
+
+	update_ui()
